d354b8f90315a8be2ee1720208c204f3118187f2
Martijn Koch <martijn_Waterreus@hotmail.com>
2022-07-24 15:28:27 +0200

    make score nullable when user is created

--- backend/database/migrations/2022_07_24_132449_create_players_table.php
+++ backend/database/migrations/2022_07_24_132449_create_players_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('players', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('score');
+            $table->string('score')->nullable();
             $table->timestamps();
         });
     }
