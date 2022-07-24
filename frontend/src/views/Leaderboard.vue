91cac8e59c30a79806b3aacd9a943a7660851dc5
Martijn Koch <martijn_Waterreus@hotmail.com>
2022-07-24 18:08:39 +0200

    get the leaderboard scores on page load

---
+++ frontend/src/views/Leaderboard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="page home background-butterflies">
+    <div class="full-width">
+      <div class="question-container">
+        <h2>Leaderboard</h2>
+
+        <ul>
+        <h2>{{user}}</h2>
+          <li v-for="(user, index) in users">
+
+              {{ user.name + ' ' +user.score }}
+         
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  data() {
+    return {
+      users: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:8000/api/leaderboard")
+      .then((response) => {
+        this.users = response.data;
+        console.log(this.users);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+});
+</script>
