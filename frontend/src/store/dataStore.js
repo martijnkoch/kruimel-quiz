5d0bd3d4d4e21454f7e8769993468749d640e9d6
Martijn Koch <martijn_Waterreus@hotmail.com>
2022-07-24 18:07:14 +0200

    save the playername in pinia store

---
+++ frontend/src/store/dataStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+export const useQuizStore = defineStore('data', {
+  state: () => {
+    return {
+      playerName: '',
+    }
+  },
+  actions: {
+  },
+  getters: {
+
+  }  
+})
